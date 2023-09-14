d9e662d081ca39cae723ab35d98556dbdc8ab804
Bruna Santiago <brunasanthiago@outlook.com>
2023-09-14 14:25:05 -0300

    feature/rota put

--- src/routes.js
+++ src/routes.js
@@ -41,5 +41,32 @@ export const routes = [
             database.insert('tasks', task)
             return res.writeHead(201).end()
         }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const { title, description } = req.body
+            if(!title || !description) return res.writeHead(400).end(
+                JSON.stringify({ error: 'Title and description are required' })
+            )
+
+            const task = database.select('tasks', { id })[0]
+
+            if(!task) return res.writeHead(404).end(
+                JSON.stringify({ error: 'Task not found' })
+            )
+
+            const updatedTask = {
+                title,
+                description,
+                updated_at: new Date()
+            }
+
+            database.update('tasks', id, updatedTask)
+
+            return res.writeHead(204).end()
+        }
     }
 ]
